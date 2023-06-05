817583988d3b486297c56f21b089dfe9b3796294
Agnieszka Białecka <aga5467@gmail.com>
2023-06-05 17:37:43 +0200

    Dodanie możliwości dodawania zadań

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState("")
     const handleSubmit = e =>{
         e.preventDefault();
-        console.log(value);
+        addTodo(value);
+        setValue("");
     }
     return (
         <form className='TodoForm' onSubmit={handleSubmit}>
-            <input type='tetx' className='todo-input' placeholder='Jakie zadanie czeka dzisiaj na ciebie ?'onChange={(e)=> setValue(e.target.value)}/>
+            <input type='tetx' className='todo-input' value={value} placeholder='Jakie zadanie czeka dzisiaj na ciebie ?' onChange={(e)=> setValue(e.target.value)}/>
             <button type='submit' className='todo-btn'>Dodaj zadanie</button>
         </form>
     )
